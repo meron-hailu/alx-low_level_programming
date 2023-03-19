1a2d62f806b8ffdd4bcd5106a400acfe8eecfbc3
Meron-hailu <merryamde@gmail.com>
2023-03-19 04:38:18 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
-int main(void) {
-  
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Description: This program prints a message to the standard output
+ * using the printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
