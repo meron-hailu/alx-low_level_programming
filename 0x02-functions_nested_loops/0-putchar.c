09506b60475fac459de7c9a5410fdff404b4e2b5
Meron-hailu <merryamde@gmail.com>
2023-03-21 12:24:39 -0700

    Update0.1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main (void)
+{
+char out[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+unsigned int i;
+for (i = 0; i < sizeof(out); i++)
+{
+putchar(out[i]);
+}
+putchar('\n');  
+return (0);
+}  
