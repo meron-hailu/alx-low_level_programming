eafdf18bf254b56bfc9729c0cee5cc1da077905b
Meron-hailu <merryamde@gmail.com>
2023-03-24 02:48:16 -0700

    Check upper case

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+/**
+* This library contains all the function prototypes of 0x04-nested functions
+*
+*/
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
